3a12fb8cb4277183ada701390db2156601ba409f
qrolande <wholetthealpacaout@yandex.ru>
2023-01-27 21:16:18 +0300

    validator is ready

--- PDFValidator.py
+++ PDFValidator.py
@@ -3,7 +3,7 @@ import fitz
 
 class PDFValidator():
 	filename = ''
-	parsed_text = {}
+	new_dict = {}
 
 	def __init__(self, filename):
 		self.filename = filename
@@ -12,10 +12,31 @@ class PDFValidator():
 
 	def parser(self):
 		text = {}
+		mydict = {}
 		with fitz.open(self.filename) as doc:
 			for page in doc:
 				text[page] = page.get_text()
-				self.parsed_text = str(text[page])
+				mydict = str(text[page]).split('\n')
 
-		return self.parsed_text.split('\n')
+		temp = []
+		for ind in range(len(mydict)):
+			temp.append(mydict[ind].split(':'))
+
+		key = []
+		value = []
+		for i in range(len(temp)):
+			if i == 0:
+				value.append('')
+			for y in range(len(temp[i])):
+				if y == 0 and temp[i][y] == ' ':
+					break
+				elif y == 0:
+					key.append(temp[i][y])
+				else:
+					value.append(temp[i][y])
+
+		self.new_dict = dict(zip(key, value))
+		print("File parsed!")
+
+		return self.new_dict
 
--- test_pdf_file.py
+++ test_pdf_file.py
@@ -1,8 +1,8 @@
-import json
 from PDFValidator import PDFValidator
 
 
 def test_compare_fields(pdf_adder):
 	file = PDFValidator(pdf_adder)
 	parsed_file = file.parser()
-	print("parsed_file =", parsed_file[1])
+	keys_value_1 = list(parsed_file.keys())
+	print("parsed_file =", keys_value_1)
